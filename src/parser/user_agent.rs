d65e5ecd12fb81824c92ec6d9213e51c8748a689
Luyi Wang <wangluyi1982@gmail.com>
2024-06-01 21:05:44 -0700

    adding the clone trait to matcher and UAParser

--- src/parser/user_agent.rs
+++ src/parser/user_agent.rs
@@ -5,7 +5,7 @@ pub enum Error {
     Regex(regex::Error),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Matcher {
     regex: regex::bytes::Regex,
     family_replacement_has_group: bool,
